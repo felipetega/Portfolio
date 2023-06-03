0ab829323331de5be7963c67754226250a3b3cc5
felipetega <felipe.crema.ribeiro@gmail.com>
2023-06-03 13:24:24 -0300

    refactor: switch icon ThemeButton fix

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({children}: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en">
       <head>
 
       </head>
--- my-app/src/components/ThemeButton.tsx
+++ my-app/src/components/ThemeButton.tsx
@@ -1,28 +1,25 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { themeChange } from 'theme-change'
-import { FiSun , FiMoon } from 'react-icons/fi';
-
-
+import React, { useState, useEffect } from 'react';
+import { themeChange } from 'theme-change';
+import { FiSun, FiMoon } from 'react-icons/fi';
 
 const ThemeButton = () => {
-
-  const [mode, setMode] = useState()
+  const [mode, setMode] = useState("light");
 
   useEffect(() => {
-    themeChange(false)
-    // 👆 false parameter is required for react project
-  }, [])
+    themeChange(false);
+    // false parameter is required for react project
+  }, []);
+
+  const handleToggleTheme = () => {
+    const newMode = mode === "dark" ? "light" : "dark";
+    setMode(newMode);
+  };
 
   return (
-    <button data-toggle-theme="dark,light,halloween" data-act-class="ACTIVECLASS">
-                {
-            mode === "dark" ?
-            <FiSun className={'fill-black'} />
-            : <FiMoon className={'fill-black'} />
-          }
+    <button data-toggle-theme="dark,light" data-act-class="ACTIVECLASS" onClick={handleToggleTheme}>
+      {mode === "dark" ? <FiMoon className="fill-black" /> : <FiSun className="fill-black" />}
     </button>
-  )
-}
+  );
+};
 
-export default ThemeButton
+export default ThemeButton;
