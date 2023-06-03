d791df738b29b2174e325e2b6e6b8c35c78fe619
felipetega <felipe.crema.ribeiro@gmail.com>
2023-06-03 18:06:18 -0300

    feat: icon theme switcher

--- my-app/src/components/ThemeButton.tsx
+++ my-app/src/components/ThemeButton.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { themeChange } from 'theme-change';
 import { FiSun, FiMoon } from 'react-icons/fi';
+import { GiNightSky, GiEvilMoon, GiSun, GiHeartStake } from "react-icons/gi";
 
 const ThemeButton = () => {
   const [mode, setMode] = useState("light");
@@ -10,18 +11,39 @@ const ThemeButton = () => {
     // false parameter is required for react project
   }, []);
 
-  const handleToggleTheme = () => {
-    const newMode = mode === "dark" ? "light" : "dark";
+  const handleToggleTheme = (event: ChangeEvent<HTMLSelectElement>) => {
+    const newMode = event.target.value;
     setMode(newMode);
   };
 
+  let selectedIcon;
+  switch (mode) {
+    case "light":
+      selectedIcon = <FiSun size={30} />;
+      break;
+    case "dark":
+      selectedIcon = <GiNightSky size={30} />;
+      break;
+    case "halloween":
+      selectedIcon = <GiEvilMoon size={30} />;
+      break;
+    case "valentine":
+      selectedIcon = <GiHeartStake size={30} />;
+      break;
+    default:
+      selectedIcon = <FiSun size={30} />;
+  }
+
   return (
-<select data-choose-theme>
-<option value="light">Light</option>
-  <option value="dark">Dark</option>
-  <option value="halloween">Halloween</option>
-  <option value="valentine">Valentine</option>
-</select>
+    <div>
+      {selectedIcon}
+      <select data-choose-theme value={mode} onChange={handleToggleTheme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="halloween">Halloween</option>
+        <option value="valentine">Valentine</option>
+      </select>
+    </div>
   );
 };
 
