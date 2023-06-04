3006eaf51870c2ead5d41a87a9354aeee75db11e
felipetega <felipe.crema.ribeiro@gmail.com>
2023-06-04 14:44:51 -0300

    refactor: Navbar, Footer style

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-<footer className="footer mt-8 p-4 bottom-0 font-medium border-black border-t-4 flex justify-between">
+<footer className="footer mt-8 p-4 bottom-0 border-black border-t-4 flex justify-between font-medium text-base">
 
 <p>Copyright © 2023 - All right reserved</p>
 
--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import Link from "next/link";
 import ThemeButton from './ThemeButton';
-import { AiFillLinkedin, AiFillGithub, AiFillLike } from 'react-icons/ai';
+import { AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 
 const Navbar = () => {
 
   return (
-    <nav className="navbar border-b-4 border-black mb-8 font-medium justify-between">
+    <nav className="navbar border-b-4 border-black mb-8 justify-between text-lg font-medium">
 
       <ul className="menu menu-horizontal px-1">
         <li><Link href={"/"}>Quem sou eu?</Link></li>
         <li><Link href={"/projetos"}>Projetos</Link></li>
         <li><Link href={"/"}>Skills</Link></li>
       </ul>
-
-      <div className="menu menu-horizontal px-1">
-          <Link href={"/"}>
-            <AiFillLike size={30}/>
-          </Link>
-      </div>
 
       <ul className="menu menu-horizontal px-1">
 
