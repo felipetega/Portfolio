aecb97e73de6b9c61b861e9660cdaae2a14c8f42
felipetega <felipe.crema.ribeiro@gmail.com>
2023-06-08 12:29:10 -0300

    styles: starting to edit icon colors

--- my-app/src/app/projetos/page.tsx
+++ my-app/src/app/projetos/page.tsx
@@ -12,31 +12,57 @@ import
 
 export default function page() {
 
-  const nextjs = [SiNextdotjs, SiTypescript, SiReact, SiTailwindcss];
+  
   const django = [SiDjango, SiPython, SiBootstrap]
   const pandas = [SiPython, SiPandas]
   const javascript = [SiJavascript, SiHtml5, SiCss3]
   
+  const NextJsIcon = () => (
+    <span className="text-black">
+      <SiNextdotjs size={30} />
+    </span>
+  );
+  
+  const TypeScriptIcon = () => (
+    <span className="text-blue-300">
+      <SiTypescript size={30} />
+    </span>
+  );
+  
+  const ReactIcon = () => (
+    <span className="text-blue-800">
+      <SiReact size={30} />
+    </span>
+  );
+  
+  const TailwindCssIcon = () => (
+    <span className="text-blue-500">
+      <SiTailwindcss size={30} />
+    </span>
+  );
+  
+  const nextjs = [SiNextdotjs, SiTypescript, SiReact, SiTailwindcss];
+  
   return (
     <div>
-      <h1 className="text-4xl font-black mb-10">Projetos - Cronologia</h1>
+      <h1 className="text-4xl font-black mb-10">Projetos</h1>
 
-      <CardSection sectionName="Stack: TypeScript, NextJS, ReactJS, TailwindCSS">
-        <SmallCard title="ToDoList" date="14/05/2023" description="CRUD completo" src="/todolist3.png" stack={nextjs} siteUrl="" showSiteButton={false}/>
+      <CardSection sectionName="">
+        <SmallCard title="ToDoList" date="14/05/2023" description="CRUD completo" src="/todolist3.png" stack={[NextJsIcon, TypeScriptIcon, ReactIcon, TailwindCssIcon]} siteUrl="" showSiteButton={false}/>
       </CardSection>
 
-      <CardSection sectionName="Stack: Python, Django, Bootstrap">
+      <CardSection sectionName="">
         <SmallCard title="Tech SeuBoné" date="05/02/2023" description="Requisitos completos" src="/seubone.png" stack={django} siteUrl="" showSiteButton={false}/>
         <SmallCard title="E-commerce" date="15/01/2023" description="Carrinho completo" src="/e-commerce.png" stack={django} siteUrl="" showSiteButton={false}/>
         <SmallCard title="Seletive" date="04/12/2022" description="CRUD completo" src="/seletive.png" stack={django} siteUrl="" showSiteButton={false}/>
       </CardSection>
 
-      <CardSection sectionName="Stack: Python, Pandas">
+      <CardSection sectionName="">
         <SmallCard title="Progresso Codewars" date="17/07/2022" description="Gerador de gráfico" src="/codewars.png" stack={pandas} siteUrl="" showSiteButton={false}/>
         <SmallCard title="Email & Report" date="10/07/2022" description="Ciência de Dados" src="/email.png" stack={pandas} siteUrl="" showSiteButton={false}/>
       </CardSection>
 
-      <CardSection sectionName="Stack: JavaScript">
+      <CardSection sectionName="">
         <SmallCard title="Jogo da Velha" date="18/09/2022" description="Jogo" src="/velha.png" stack={javascript} siteUrl="https://felipetega.github.io/Jogo-Da-Velha/" showSiteButton={true}/>
         <SmallCard title="Cursos Realizados" date="12/06/2022" description="Gerador de gráfico" src="/cursos.png" stack={javascript} siteUrl="https://felipetega.github.io/CursosRealizados-CHART.JS/" showSiteButton={true}/>
       </CardSection>
