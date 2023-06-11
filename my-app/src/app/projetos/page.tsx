b1ab87627641b3dca1b8546e2e55b997882535a0
felipetega <felipe.crema.ribeiro@gmail.com>
2023-06-11 09:59:35 -0300

    style: tech icons

--- my-app/src/app/projetos/page.tsx
+++ my-app/src/app/projetos/page.tsx
@@ -1,6 +1,7 @@
 //import LargeCard from "@/components/cards/LargeCard";
 import SmallCard from "@/components/cards/SmallCard";
 import CardSection from "@/components/cards/CardSection";
+
 import 
 { 
   SiNextdotjs, SiTypescript, SiReact, SiTailwindcss,
@@ -13,58 +14,87 @@ import
 export default function page() {
 
   
-  const django = [SiDjango, SiPython, SiBootstrap]
-  const pandas = [SiPython, SiPandas]
-  const javascript = [SiJavascript, SiHtml5, SiCss3]
   
-  const NextJsIcon = () => (
-    <span className="text-black">
-      <SiNextdotjs size={30} />
-    </span>
+  const nextJsStack = () => (
+    <>
+      <div className="tooltip" data-tip="NextJS">
+        <SiNextdotjs size={40} fill="black" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="Typescript">
+        <SiTypescript size={40} fill="#007ACC" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="ReactJS">
+        <SiReact size={40} fill="#61DAFB" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="TailwindCSS">
+        <SiTailwindcss size={40} fill="#06B6D4" className="mx-2" />
+      </div>
+    </>
   );
   
-  const TypeScriptIcon = () => (
-    <span className="text-blue-300">
-      <SiTypescript size={30} />
-    </span>
+  const djangoStack = () => (
+    <>
+      <div className="tooltip" data-tip="Django">
+        <SiDjango size={40} fill="#092E20" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="Python">
+        <SiPython size={40} fill="black" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="Bootstrap">
+        <SiBootstrap size={40} fill="#7952B3" className="mx-2" />
+      </div>
+    </>
   );
   
-  const ReactIcon = () => (
-    <span className="text-blue-800">
-      <SiReact size={30} />
-    </span>
+  const pandasStack = () => (
+    <>
+      <div className="tooltip" data-tip="Python">
+        <SiPython size={40} fill="black" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="Pandas">
+        <SiPandas size={40} fill="#150458" className="mx-2" />
+      </div>
+    </>
   );
   
-  const TailwindCssIcon = () => (
-    <span className="text-blue-500">
-      <SiTailwindcss size={30} />
-    </span>
+  const javascriptStack = () => (
+    <>
+      <div className="tooltip" data-tip="Javascript">
+        <SiJavascript size={40} fill="#F7DF1E" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="HTML5">
+        <SiHtml5 size={40} fill="#E34F26" className="mx-2" />
+      </div>
+      <div className="tooltip" data-tip="CSS3">
+        <SiCss3 size={40} fill="#1572B6" className="mx-2" />
+      </div>
+    </>
   );
   
-  const nextjs = [SiNextdotjs, SiTypescript, SiReact, SiTailwindcss];
   
+
   return (
     <div>
       <h1 className="text-4xl font-black mb-10">Projetos</h1>
 
       <CardSection sectionName="">
-        <SmallCard title="ToDoList" date="14/05/2023" description="CRUD completo" src="/todolist3.png" stack={[NextJsIcon, TypeScriptIcon, ReactIcon, TailwindCssIcon]} siteUrl="" showSiteButton={false}/>
+        <SmallCard title="ToDoList" date="14/05/2023" description="CRUD completo" src="/todolist3.png" stack={[nextJsStack]} siteUrl="" showSiteButton={false}/>
       </CardSection>
 
       <CardSection sectionName="">
-        <SmallCard title="Tech SeuBoné" date="05/02/2023" description="Requisitos completos" src="/seubone.png" stack={django} siteUrl="" showSiteButton={false}/>
-        <SmallCard title="E-commerce" date="15/01/2023" description="Carrinho completo" src="/e-commerce.png" stack={django} siteUrl="" showSiteButton={false}/>
-        <SmallCard title="Seletive" date="04/12/2022" description="CRUD completo" src="/seletive.png" stack={django} siteUrl="" showSiteButton={false}/>
+        <SmallCard title="Tech SeuBoné" date="05/02/2023" description="Requisitos completos" src="/seubone.png" stack={[djangoStack]} siteUrl="" showSiteButton={false}/>
+        <SmallCard title="E-commerce" date="15/01/2023" description="Carrinho completo" src="/e-commerce.png" stack={[djangoStack]} siteUrl="" showSiteButton={false}/>
+        <SmallCard title="Seletive" date="04/12/2022" description="CRUD completo" src="/seletive.png" stack={[djangoStack]} siteUrl="" showSiteButton={false}/>
       </CardSection>
 
       <CardSection sectionName="">
-        <SmallCard title="Progresso Codewars" date="17/07/2022" description="Gerador de gráfico" src="/codewars.png" stack={pandas} siteUrl="" showSiteButton={false}/>
-        <SmallCard title="Email & Report" date="10/07/2022" description="Ciência de Dados" src="/email.png" stack={pandas} siteUrl="" showSiteButton={false}/>
+        <SmallCard title="Progresso Codewars" date="17/07/2022" description="Gerador de gráfico" src="/codewars.png" stack={[pandasStack]} siteUrl="" showSiteButton={false}/>
+        <SmallCard title="Email & Report" date="10/07/2022" description="Ciência de Dados" src="/email.png" stack={[pandasStack]} siteUrl="" showSiteButton={false}/>
       </CardSection>
 
       <CardSection sectionName="">
-        <SmallCard title="Jogo da Velha" date="18/09/2022" description="Jogo" src="/velha.png" stack={javascript} siteUrl="https://felipetega.github.io/Jogo-Da-Velha/" showSiteButton={true}/>
-        <SmallCard title="Cursos Realizados" date="12/06/2022" description="Gerador de gráfico" src="/cursos.png" stack={javascript} siteUrl="https://felipetega.github.io/CursosRealizados-CHART.JS/" showSiteButton={true}/>
+        <SmallCard title="Jogo da Velha" date="18/09/2022" description="Jogo" src="/velha.png" stack={[javascriptStack]} siteUrl="https://felipetega.github.io/Jogo-Da-Velha/" showSiteButton={true}/>
+        <SmallCard title="Cursos Realizados" date="12/06/2022" description="Gerador de gráfico" src="/cursos.png" stack={[javascriptStack]} siteUrl="https://felipetega.github.io/CursosRealizados-CHART.JS/" showSiteButton={true}/>
       </CardSection>
 
   </div>
